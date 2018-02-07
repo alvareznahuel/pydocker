739a09f260f65dedb5cf591a1b3abde62a8f9321
alvareznahuel <alvarez.nahuel@gmail.com>
2018-02-06 23:40:02 -0300

    Ajustes finales para el DEBUG REMOTO.

--- hello.py
+++ hello.py
@@ -1,6 +1,16 @@
+# Inicio del DEBUG REMOTO.
+import time
 import ptvsd
 ptvsd.enable_attach("my_secret", address = ('0.0.0.0', 3000))
-#Enable the below line of code only if you want the application to wait untill the debugger has attached to it
+print("Esperando conexion del debbuger...")
 ptvsd.wait_for_attach()
+ptvsd.break_into_debugger()
+# Inicio de la logica del programa.
 for i in range(10):
     print("Hola mundo %d!" % i)
+for j in range(20):
+    print("Chau mundo %d!" % j)
+# Finalizacion del DEBUG REMOTO.
+print("Esperando desconexion del debbuger...")
+while ptvsd.is_attached():
+    time.sleep(0.5)
